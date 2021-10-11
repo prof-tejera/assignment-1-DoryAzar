a3a398a6f6387f0c9a0feb1aeeb111d1622f186e
Dory Azar <dory.azar@gmail.com>
2021-10-11 19:23:49 -0400

    Deploy version 0.3

--- src/components/timers/Tabata.js
+++ src/components/timers/Tabata.js
@@ -4,12 +4,11 @@ import Timer from "../generic/Timer";
 class Tabata extends Component {
   render() {
     const settings = [
-      {label: "Start Time", value: "00:45:00", placeholder: "Start the timer at this time"},
-      {label: "Stop Time", value:"00:00:00", placeholder: "Stop the timer at this time"},
+      {label: "Work Time", value: "00:45:00", placeholder: "Start the timer at this time"},
       {label: "Rest Time", value:"00:10:00", placeholder: "Timer will rest for that duration"},
       {label: "Rounds", value:"2", placeholder: "Stops after that many rounds"}
     ]
-    return <Timer statusMessage="Round 1 of 2" startTime="00:45:00" settings={settings} />;
+    return <Timer statusMessage="Round 1 of 2  - Work" startTime="00:45:00" settings={settings} />;
   }
 }
 
